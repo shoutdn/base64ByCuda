900ec2cc244d7b15772bf0a3f487196cd1c5dd37
liu <1224998548@qq.com>
2023-04-06 11:25:28 +0800

    testing

--- kernel.cu
+++ kernel.cu
@@ -6,11 +6,11 @@
 
 cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
 
-__global__ void addKernel(int *c, const int *a, const int *b)
-{
-    int i = threadIdx.x;
-    c[i] = a[i] + b[i];
-}
+//__global__ void addKernel(int *c, const int *a, const int *b)
+//{
+//    int i = threadIdx.x;
+//    c[i] = a[i] + b[i];
+//}
 
 int main()
 {
